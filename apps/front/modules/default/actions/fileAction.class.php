b634a5020be0443c45468b0128e32cac5c29a34d
Zoltan Baldaszti <zoltan.baldaszti@ents24.com>
2016-08-24 14:09:13 +0100

    Issue #3: file ordering fixed

--- apps/front/modules/default/actions/fileAction.class.php
+++ apps/front/modules/default/actions/fileAction.class.php
@@ -31,24 +31,11 @@ class fileAction extends crewAction
       $options['ignore-all-space'] = true;
     }
 
-    $previousFiles = FileQuery::create()
-      ->select(array('Id', 'Filename'))
+    // use the same list as fileListAction.class.php
+    $files = FileQuery::create()
       ->filterByBranchId($this->file->getBranchId())
-      ->filterByFilename($this->file->getFilename(), Criteria::LESS_THAN)
-      ->filterByIsBinary(false)
-      ->orderByFilename(Criteria::DESC)
       ->find()
     ;
-
-    $nextFiles = FileQuery::create()
-      ->select(array('Id', 'Filename'))
-      ->filterByBranchId($this->file->getBranchId())
-      ->filterByFilename($this->file->getFilename(), Criteria::GREATER_THAN)
-      ->filterByIsBinary(false)
-      ->orderByFilename(Criteria::ASC)
-      ->find()
-    ;
-
 
     $commitFrom        = $request->getParameter('from', $this->branch->getCommitReference());
     $commitTo          = $request->getParameter('to', $this->branch->getLastCommit());
@@ -74,8 +61,29 @@ class fileAction extends crewAction
       false
     );
     
-    $this->previousFileId = $this->findClosestFileId($previousFiles, $modifiedFiles);
-    $this->nextFileId     = $this->findClosestFileId($nextFiles, $modifiedFiles);
+    $lastId = $this->previousFileId = $this->nextFileId = null;
+    $getNext = false;
+    // iterate on the list only once and get both previous and next ids
+    foreach ($files as $file)
+    {
+      /** @var File $file  */
+      if (!isset($modifiedFiles[$file->getFilename()]))
+      {
+        continue;
+      }
+      if ($getNext) {
+        // save the current id as next id
+        $this->nextFileId = $file->getId();
+        break;
+      }
+      if ($this->file->getId() == $file->getId()) {
+        // save the last valid file's id as previous id
+        $this->previousFileId = $lastId;
+        // set flag so next iteration with a valid file will know what to do
+        $getNext = true;
+      }
+      $lastId = $file->getId();
+    }
 
     $this->fileContentLines = $this->gitCommand->getShowFileFromBranch(
       $this->repository->getGitDir(),
@@ -101,24 +109,4 @@ class fileAction extends crewAction
     }
   }
 
-  /**
-   * @param Traversable $files
-   * @param array       $modifiedFiles
-   *
-   * @return null
-   */
-  private function findClosestFileId(Traversable $files, array $modifiedFiles)
-  {
-    $fileId = null;
-    foreach ($files as $file)
-    {
-      if (isset($modifiedFiles[$file['Filename']]))
-      {
-        return $file['Id'];
-      }
-    }
-    
-    return null;
-  }
-  
 }
